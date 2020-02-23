1d025745d754e69a76753d5645cd1585ebb9bb02
Serguei Rodriguez <sjosevich@gmail.com>
2020-02-22 21:15:22 -0700

    Changed the model name and added the post and the read article to the controllers file

--- controller/controller.js
+++ controller/controller.js
@@ -5,7 +5,7 @@ var path = require("path");
 var request = require("request");
 var cheerio = require("cheerio");
 
-var Note = require("../models/Note.js");
+var Comment = require("../models/Comment.js");
 var Article = require("../models/Article.js");
 
 var axios = require("axios");
@@ -77,41 +77,93 @@ router.get("/scrape", function(req, res) {
       }
     });
   });
-  
-  // Route for grabbing a specific Article by id, populate it with it's note
-  router.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-      Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
+
+  router.get("/clearAll", function(req, res) {
+    Article.deleteMany({}, function(err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("removed all articles");
+      }
+    });
+    res.redirect("/articles-json");
   });
   
-  // Route for saving/updating an Article's associated Note
-  router.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-       Note.create(req.body)
-      .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
+  router.get("/articles-json", function(req, res) {
+    Article.find({}, function(err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(doc);
+      }
+    });
+  });
+
+  router.get("/readArticle/:id", function(req, res) {
+    var articleId = req.params.id;
+    var hbsObj = {
+      article: [],
+      body: []
+    };
+  
+    Article.findOne({ _id: articleId })
+      .populate("comment")
+      .exec(function(err, doc) {
+        if (err) {
+          console.log("Error: " + err);
+        } else {
+          hbsObj.article = doc;
+          var link = doc.link;
+          request(link, function(error, response, html) {
+            var $ = cheerio.load(html);
+  
+            $("p").each(function(i, element) {
+              hbsObj.body = $(this)
+                .children(".c-entry-content")
+                .children("p")
+                .text();
+  
+              res.render("article", hbsObj);
+              return false;
+            });
+          });
+        }
       });
+  });
+
+
+  router.post("/comment/:id", function(req, res) {
+    var user = req.body.name;
+    var content = req.body.comment;
+    var articleId = req.params.id;
+  
+    var commentObj = {
+      name: user,
+      body: content
+    };
+  
+    var newComment = new Comment(commentObj);
+  
+    newComment.save(function(err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(doc._id);
+        console.log(articleId);
+  
+        Article.findOneAndUpdate(
+          { _id: req.params.id },
+          { $push: { comment: doc._id } },
+          { new: true }
+        ).exec(function(err, doc) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect("/readArticle/" + articleId);
+          }
+        });
+      }
+    });
   });
 
   module.exports = router;
