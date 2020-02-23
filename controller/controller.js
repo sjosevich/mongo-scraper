1a8613154984fc1e714a516e79808c868a7d6247
Serguei Rodriguez <sjosevich@gmail.com>
2020-02-22 19:29:12 -0700

    Created the handlebars files and the controller.js file

---
+++ controller/controller.js
@@ -0,0 +1,117 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var Note = require("../models/Note.js");
+var Article = require("../models/Article.js");
+
+var axios = require("axios");
+
+// Routes
+
+router.get("/", function(req, res) {
+    res.redirect("/articles");
+  });
+
+// A GET route for scraping the echoJS website
+router.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("http://www.echojs.com/").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+      var titlesArray = [];
+  
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("article h2").each(function(i, element) {
+        var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      if (result.title !== "" && result.link !== "") {
+        if (titlesArray.indexOf(result.title) == -1) {
+          titlesArray.push(result.title);
+
+          Article.count({ title: result.title }, function(err, test) {
+            if (test === 0) {
+              var entry = new Article(result);
+
+              entry.save(function(err, doc) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log(doc);
+                }
+              });
+            }
+          });
+        } else {
+          console.log("Article already exists.");
+        }
+      } else {
+        console.log("Not saved to DB, missing data");
+      }
+    });
+    res.redirect("/");
+  });
+});
+  
+  // Route for getting all Articles from the db
+ router.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    Article.find()
+    .sort({ _id: -1 })
+    .exec(function(err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        var artcl = { article: doc };
+        res.render("index", artcl);
+      }
+    });
+  });
+  
+  // Route for grabbing a specific Article by id, populate it with it's note
+  router.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+      Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  router.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+       Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  module.exports = router;
---
+++ server.js
@@ -0,0 +1,46 @@
+//dependencies
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+//initialize Express app
+var express = require("express");
+var app = express();
+
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+//Require set up handlebars
+var exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/scraper", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
+
+var routes = require("./controller/controller.js");
+app.use("/", routes);
+//Create localhost port
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
